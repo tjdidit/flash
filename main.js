57166632a31609a91f37abc231b58d7d6556ed9c
tj <loudontj@gmail.com>
2025-01-19 18:58:46 -0500

    change

--- main.js
+++ main.js
@@ -6,14 +6,31 @@ const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, ()=>{
-	console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`);
 }); 
 
 app.get('/', (req, res)=> {
-	res.send('hello world!');
+    console.log('Got a web request');
+    res.send('hello world!');
 });
 
 io.on('connection', (socket)=>{
-	console.log(`socketID: ${socket.id}`);
+    console.log(`New client connected!`);
+    console.log(`socketID: ${socket.id}`);
 
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
+
+    socket.on('message', (data) => {
+        console.log(`Received message from ${socket.id}:`, data);
+        // Broadcast to other clients
+        socket.broadcast.emit('message', {
+            ...data,
+            senderId: socket.id
+        });
+    });
 });
+
+// Log when server starts
+console.log('Starting server...');
